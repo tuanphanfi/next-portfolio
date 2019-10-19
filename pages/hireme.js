0f8a40d95013a89a47b037a343382ef96d81cecd
Tuan Phan <tuanphan.fi@gmail.com>
2019-10-19 22:45:09 +0300

    nextjs first deploy on zeit.co

---
+++ pages/hireme.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+const HireMe = () => (
+  <Layout title='Hire Me'>
+    <p>
+      You can hire me at{' '}
+      <a href='mailto:tuanphan.fi@gmail.com'>tuanphan.fi@gmail.com</a>
+    </p>
+  </Layout>
+);
+
+export default HireMe;
